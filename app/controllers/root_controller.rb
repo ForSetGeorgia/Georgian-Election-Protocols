0c5284c4f0e3389329226c2b2494cc69666cbffd
jason <jason.addie@gmail.com>
2013-10-23 11:57:16 +0400

    added overall stats to home page

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,6 +1,7 @@
 class RootController < ApplicationController
 
   def index
+    @overall_stats = DistrictPrecinct.overall_stats
   end
 
 
--- app/models/district_precinct.rb
+++ app/models/district_precinct.rb
@@ -1,5 +1,62 @@
 class DistrictPrecinct < ActiveRecord::Base
 
+  attr_accessor :num_precincts, :num_protocols_found, :num_protocols_missing, :num_protocols_not_entered, :num_protocols_validated
+
+
+  #######################
+  # flag descriptions
+  #######################
+  # has_protocol
+  # - 0 = default value
+  # - 1 = set to true when scrapper indicates that it found the protocol
+  
+  # is_validated
+  # - 0 = default value
+  # - 1 = set to true two crowd datum records have matched and the data has been moved to pres table
+  
+  #######################
+
+
+  #######################
+  ## stats
+  #######################
+
+  # get the following:
+  # total districts (#), total precincts (#), protocols found (#/%), protocols missing (#/%), protocols not entered (#/%), protocols validated (#/%) 
+  def self.overall_stats
+    stats = nil
+    district_count = DistrictPrecinct.select('distinct district_id').count
+    
+    sql = "select count(*) as num_precincts, sum(has_protocol) as num_protocols_found, (count(*) - sum(has_protocol)) as num_protocols_missing, "
+    sql << "sum(if(has_protocol = 1 and is_validated = 0, 1, 0)) as num_protocols_not_entered, sum(if(has_protocol = 1 and is_validated = 1, 1, 0)) as num_protocols_validated "
+    sql << "from district_precincts "
+
+    data = find_by_sql(sql)
+    
+    if data.present?
+      data = data.first
+      stats = Hash.new
+      stats[:districts] = format_number(district_count)
+      stats[:precincts] = format_number(data[:num_precincts])
+      stats[:protocols_missing] = Hash.new
+      stats[:protocols_missing][:number] = format_number(data[:num_protocols_missing])
+      stats[:protocols_missing][:percent] = format_percent(100*data[:num_protocols_missing]/data[:num_precincts].to_f)
+      stats[:protocols_found] = Hash.new
+      stats[:protocols_found][:number] = format_number(data[:num_protocols_found])
+      stats[:protocols_found][:percent] = format_percent(100*data[:num_protocols_found]/data[:num_precincts].to_f)
+      stats[:protocols_not_entered] = Hash.new
+      stats[:protocols_not_entered][:number] = format_number(data[:num_protocols_not_entered])
+      stats[:protocols_not_entered][:percent] = format_percent(100*data[:num_protocols_not_entered]/data[:num_protocols_found].to_f)
+      stats[:protocols_validated] = Hash.new
+      stats[:protocols_validated][:number] = format_number(data[:num_protocols_validated])
+      stats[:protocols_validated][:percent] = format_percent(100*data[:num_protocols_validated]/data[:num_protocols_found].to_f)
+    end    
+    return stats
+  end
+
+  #######################
+  #######################
+
   # creates array of districts/precincts that are missing protocols
   #format: [ {district_id => x, precincts => [ {id => x, found => false }, ...  ] }, .... ]
   def self.missing_protocols
@@ -67,6 +124,19 @@ class DistrictPrecinct < ActiveRecord::Base
     end
 
     return success
+  end
+
+
+
+  protected 
+  
+  
+  def self.format_number(number)
+    ActionController::Base.helpers.number_with_delimiter(ActionController::Base.helpers.number_with_precision(number))
+  end
+
+  def self.format_percent(number)
+    ActionController::Base.helpers.number_to_percentage(ActionController::Base.helpers.number_with_precision(number))
   end
 
 end
