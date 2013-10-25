aee221b9452badc7757d28a631d041721144d809
vazha-asatiani <vazha.asatiani@jumpstart.ge>
2013-10-25 17:07:07 +0400

    schema altered

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131024120212) do
+ActiveRecord::Schema.define(:version => 20131025125917) do
 
   create_table "crowd_data", :force => true do |t|
     t.integer  "district_id"
