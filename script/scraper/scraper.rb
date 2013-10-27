9c0dcaafce5617cadaf031ba219f6ad9c3efc289
root <root@eric-work.(none)>
2013-10-27 15:50:51 +0400

    updated scraper to account for amendments

--- script/scraper/scraper.rb
+++ script/scraper/scraper.rb
@@ -12,13 +12,16 @@ require './vpn_switcher.rb'
 
 # main variables
 
+counter_max = 20
+
 cec_base_url = "results2012.cec.gov.ge"
-election = "major"
+election = "prop"
 
 app_base_url = "protocols.jumpstart.ge"
 app_get_uri = "/en/json/missing_protocols"
 app_post_uri = "/en/json/mark_found_protocols"
 protocol_dir = "/root/protocols/"
+#protocol_dir = "/home/eric/Desktop/cec_scraper/"
 remote_server = "protocols@epsilon.jumpstart.ge"
 remote_dir = "~/Protocols/shared/system/protocols/"
 
@@ -48,21 +51,27 @@ else
 
   # get list of missing precincts
 
-  file = Net::HTTP.get(app_base_url, app_get_uri)
-  districts = JSON.parse(file).sample(3)
-
+#  file = Net::HTTP.get(app_base_url, app_get_uri)
+#  districts = JSON.parse(file)
+  
+  counter = 0 
+  
   districts.each do |district|
-    district_id = district["district_id"]
-     
+
+#    district_id = district["district_id"]
+    district_id = "15"
+         
     # create protocol directory if it doesn't exist
     
     district_dir = protocol_dir + district_id.to_s
     Dir.mkdir(district_dir) unless File.exists?(district_dir)
 
-    precincts = district["precincts"].sample(3)
-    
+#    precincts = district["precincts"]
+    precincts = ["1", "2", "3", "4", "5", "7", "9", "10", "12", "14", "15", "16", "17", "18", "19", "20"]    
     precincts.each do |precinct|
-      precinct_id = precinct["id"]
+    
+      precinct_id = precinct
+      #precinct_id = precinct["id"]
       
       cec_page_uri = "/#{election}/oqmi_" + 
                  district_id.to_s + "_" + 
@@ -74,43 +83,81 @@ else
       response = Net::HTTP.get_response(URI("http://" + cec_base_url + cec_page_uri))
     
       if response.code.to_s == "200"
-        puts "Page found."
+        puts "Page http://" + cec_base_url + cec_page_uri + " found."
         page = Net::HTTP.get(cec_base_url, cec_page_uri)
         page_contents = Nokogiri::HTML(page)
         
-        # if protocol exists get protocol
+        # if protocol and/or amendment exists get protocol
         
-        unless page_contents.css("img")[0].attribute("src").nil?
-          puts "#{district_id}-#{precinct_id} image found"
-          image_uri = page_contents.css("img")[0].attribute("src").to_s.sub("..", "")
-          image_url = "http://" + cec_base_url + image_uri
-          filename = district_id.to_s + "-" + precinct_id.to_s + ".jpg"
-          
-          if remote_server_up?(image_url)
-            open("#{district_dir}/#{filename}", 'wb') do |file|
-              file << open(image_url).read
-            end # open file
-            puts "#{district_id}-#{precinct_id} saved to drive."
-            
-            # copy file to remote server
-            puts "Copying protocol to application server."
-            `scp #{district_dir}/#{filename} #{remote_server}:#{remote_dir}#{district_id}`
-            puts "Copied file to application server."
-            
-            # update array so that found = true
-            precinct["found"] = true
-          else
-            puts "#{district_id}-#{precinct_id} image not found."
-          end
+        if page_contents.css("img").nil?
+          puts "No images exist on #{cec_base_url + cec_page_uri}."
         else
-          puts "#{district_id}-#{precinct_id} image not found."
-        end # if image exists
+          if page_contents.css("img")[0].attribute("src").nil?
+            puts "No protocol image exists."
+          else
+            proto_exist = true
+            puts "Protocol #{district_id}-#{precinct_id} image found."
+            prot_image_uri = page_contents.css("img")[0].attribute("src").to_s.sub("..", "")
+            prot_image_url = "http://" + cec_base_url + prot_image_uri
+            prot_filename = district_id.to_s + "-" + precinct_id.to_s + ".jpg"
+          end
+          
+          if page_contents.css("img")[1].nil?
+            puts "No amended image exists."
+          else
+            amend_exist = true
+            puts "*** Amended #{district_id}-#{precinct_id} image found. ***"
+            amend_image_uri = page_contents.css("img")[1].attribute("src").to_s.sub("..", "")
+            amend_image_url = "http://" + cec_base_url + amend_image_uri
+            amend_filename = district_id.to_s + "-" + precinct_id.to_s + "amended.jpg"
+          end
+          
+          if remote_server_up?(prot_image_url)
+          
+            if proto_exist
+              open("#{district_dir}/#{prot_filename}", 'wb') do |pfile|
+                pfile << open(prot_image_url).read
+              end # open file
+              puts "Protocol #{district_id}-#{precinct_id} saved to local drive."
+            
+              # copy file to remote server
+              puts "Copying protocol to application server."
+              `scp #{district_dir}/#{prot_filename} #{remote_server}:#{remote_dir}#{district_id}`
+              puts "Copied file to application server."
+              counter += 1
+            else
+              puts "Protocol #{district_id}-#{precinct_id} image not found."
+            end # proto_exist
+            
+            if amend_exist
+              open("#{district_dir}/#{amend_filename}", 'wb') do |afile|
+                afile << open(amend_image_url).read
+              end # open file
+              puts "Amended #{district_id}-#{precinct_id} saved to local drive."
+            
+              # copy file to remote server
+              puts "Copying amended protocol to application server."
+              `scp #{district_dir}/#{amend_filename} #{remote_server}:#{remote_dir}#{district_id}`
+              puts "*** Copied file to application server. ***"
+            else
+              puts "Amended #{district_id}-#{precinct_id} image not found."
+            end # amend_exists
+            
+          else
+            puts "Remote server is not up."
+          end # check if remote server is up
+          
+        end # no images found on page  
+      
       else
         puts "#{district_id}-#{precinct_id} page not found."
       end # if page exists
-      
+    
+    puts "Counter: #{counter}"
+    break if counter > counter_max
     end # precincts loop
 
+  break if counter > counter_max
   end # districts loop
 
   end_time = Time.now
