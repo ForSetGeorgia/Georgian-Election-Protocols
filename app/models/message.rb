63c38da82df6e900cf59f008731917e2a146ae91
jason <jason.addie@gmail.com>
2016-10-06 01:28:01 +0400

    bug fix - legacy code

--- app/models/message.rb
+++ app/models/message.rb
@@ -11,8 +11,8 @@ class Message
 	attribute :url_id
 	attribute :bcc
 	attribute :locale, :default => I18n.locale
-  
-	attr_accessible :name, :email, :message, :message2, :subject, :url, :bcc, :url_id, :locale
+
+	# attr_accessible :name, :email, :message, :message2, :subject, :url, :bcc, :url_id, :locale
 
   validates_presence_of :email
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
