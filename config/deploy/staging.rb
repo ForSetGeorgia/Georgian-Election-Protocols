d8853a11a285039d3804c0ed00e4d9b9d8859984
jason <jason.addie@gmail.com>
2017-10-10 13:45:59 +0400

    update staging deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,13 +1,13 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-server "alpha.jumpstart.ge", :web, :app, :db, primary: true # server where app is located
-set :application, "Protocols-Staging" # unique name of application
-set :user, "protocols-staging" # name of user on server
+server "beta.forset.ge", :web, :app, :db, primary: true # server where app is located
+set :application, "Protocols" # unique name of application
+set :user, "deploy" # name of user on server
 set :ngnix_conf_file_loc, "staging/nginx.conf" # location of nginx conf file
 set :unicorn_init_file_loc, "staging/unicorn_init.sh" # location of unicor init shell file
-set :github_account_name, "JumpStartGeorgia" # name of accout on git hub
-set :github_repo_name, "Crowd-Source-Protocols" # name of git hub repo
-set :git_branch_name, "master" # name of branch to deploy
+set :github_account_name, "ForSetGeorgia" # name of accout on git hub
+set :github_repo_name, "Georgian-Election-Protocols" # name of git hub repo
+set :git_branch_name, "2017" # name of branch to deploy
 set :rails_env, "staging" # name of environment: production, staging, ...
 ##################################
--- config/deploy/staging/unicorn.rb
+++ config/deploy/staging/unicorn.rb
@@ -1,8 +1,8 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-root = "/home/protocols-staging/Protocols-Staging/current" # path to application current folder
-sock_name = "unicorn_Protocols-Staging" # must be unique name with no '.'
+root = "/home/deploy/Protocols/current" # path to application current folder
+sock_name = "unicorn_Protocols" # must be unique name with no '.'
 port_num = 8116 # must be a unique port number for this application
 tout = 30 # time in seconds before process dies - need a long time for data uploads
 ##################################
@@ -14,5 +14,5 @@ stdout_path "#{root}/log/unicorn.log"
 
 listen "/tmp/#{sock_name}.sock"
 listen port_num, :tcp_nopush => true
-worker_processes 2
+worker_processes 1
 timeout tout
