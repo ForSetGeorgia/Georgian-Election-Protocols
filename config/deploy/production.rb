9f5aa03ef13414f0c1c5ee1540f44f450e355348
jason <jason.addie@gmail.com>
2017-10-14 01:11:20 +0400

    update prod deploy; turn off ssl

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,13 @@
 ##################################
 ##### SET THESE VARIABLES ########
 ##################################
-server "epsilon.jumpstart.ge", :web, :app, :db, primary: true # server where app is located
+server "gamma.forset.ge", :web, :app, :db, primary: true # server where app is located
 set :application, "Protocols" # unique name of application
 set :user, "protocols"# name of user on server
 set :ngnix_conf_file_loc, "production/nginx.conf" # location of nginx conf file
 set :unicorn_init_file_loc, "production/unicorn_init.sh" # location of unicor init shell file
-set :github_account_name, "JumpStartGeorgia" # name of accout on git hub
-set :github_repo_name, "Crowd-Source-Protocols" # name of git hub repo
-set :git_branch_name, "master" # name of branch to deploy
+set :github_account_name, "ForSetGeorgia" # name of accout on git hub
+set :github_repo_name, "Georgian-Election-Protocols" # name of git hub repo
+set :git_branch_name, "2017" # name of branch to deploy
 set :rails_env, "production" # name of environment: production, staging, ...
 ##################################
