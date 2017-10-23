ed8d7669b95e0342cefa7d7d7a90b839a93d4862
jason <jason.addie@gmail.com>
2017-10-23 12:36:39 +0400

    bug fix - var name

--- app/models/crowd_queue.rb
+++ app/models/crowd_queue.rb
@@ -39,10 +39,10 @@ class CrowdQueue < ActiveRecord::Base
       logger.debug "))) - found match, marking as finished"
       sql = "election_id = :election and user_id = :user and district_id = :district and precinct_id = :precinct"
       if major_district_id.present?
-        sql << " and major_district_id = :major_district_id"
+        sql << " and major_district_id = :major_district"
       end
       CrowdQueue.where(["#{sql} and is_finished is null",
-                election: election_id, user: user_id, district: district_id, precinct: precinct_id, major_district_: major_district_id])
+                election: election_id, user: user_id, district: district_id, precinct: precinct_id, major_district: major_district_id])
               .update_all(:is_finished => true)
     end
   end
